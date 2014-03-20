ef2aeee6934ebf697e13374e7c8600c9562c5cb3
Eelco Wiersma <eelco@pagebakers.nl>
2014-03-20 16:54:03 +0100

    Restart apache after config update

--- manifests/ui.pp
+++ manifests/ui.pp
@@ -12,7 +12,8 @@ class kazoo::ui {
   
   exec { 'set-default-document': 
     command => "/bin/sed -i 's#/var/www/html#/var/www/html/kazoo-ui#g' /etc/httpd/conf/httpd.conf",
-    require => Package['kazoo-ui']
+    require => Package['kazoo-ui'],
+    notify => Service['httpd']
   }
   
   service { 'httpd':
