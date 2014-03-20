2d1300f349f5de91bd408b1459d31598aa21682a
Eelco Wiersma <eelco@pagebakers.nl>
2014-03-20 16:02:41 +0100

    Removed ruby provider

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 class kazoo {
-  
-  file { '/usr/lib/ruby/site_ruby/1.8/puppet/provider.rb': ensure => 'present', source => 'puppet:///modules/kazoo/provider.rb' }
+
   file { '/etc/yum.repos.d/epel.repo': ensure => 'present', source => 'puppet:///modules/kazoo/epel.repo' }
   file { '/etc/yum.repos.d/2600hz.repo': ensure => 'present', source => 'puppet:///modules/kazoo/2600hz.repo' }
   file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6': ensure => 'present', source => 'puppet:///modules/kazoo/RPM-GPG-KEY-EPEL-6' }
