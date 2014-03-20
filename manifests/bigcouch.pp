45b631205d29c3da8b826719f4438964a9d04f84
Eelco Wiersma <eelco@pagebakers.nl>
2014-03-20 22:33:49 +0100

    Little refactoring, removed rabbitmq variable

--- manifests/bigcouch.pp
+++ manifests/bigcouch.pp
@@ -1,7 +1,6 @@
 class kazoo::bigcouch (
   
   $cookie = 'change_me',
-  $rabbitmq_ip = '127.0.0.1',
   $is_primary = false,
   $bigcouch_nodes = []
   
@@ -9,7 +8,7 @@ class kazoo::bigcouch (
   
   require kazoo
   
-  package { 'kazoo-bigcouch-R15B': ensure => installed, require => File['/etc/yum.repos.d/2600hz.repo'] }
+  package { 'kazoo-bigcouch-R15B': ensure => 'installed', require => File['/etc/yum.repos.d/2600hz.repo'] }
 
   define add_node ($ip, $hostname) {
     unless $hostname == $fqdn {
@@ -21,11 +20,11 @@ class kazoo::bigcouch (
   }
   
   if $is_primary == true {
-    create_resources(add_node, $bigcouch_nodes )
+    create_resources(add_node, $bigcouch_nodes)
   }
   
   service { 'bigcouch':
-    ensure => running,
+    ensure => 'running',
     enable => true,
     require => Package['kazoo-bigcouch-R15B']
   }
