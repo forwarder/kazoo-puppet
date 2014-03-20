de88af6e142acd63e65e7abd0e04dc5aa6a256d0
Eelco Wiersma <eelco@pagebakers.nl>
2014-03-20 17:01:37 +0100

    freeswitch template fix

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,7 @@
 class kazoo {
 
-  #file { '/etc/yum.repos.d/epel.repo': ensure => 'present', source => 'puppet:///modules/kazoo/epel.repo' }
   file { '/etc/yum.repos.d/2600hz.repo': ensure => 'present', source => 'puppet:///modules/kazoo/2600hz.repo' }
-  #file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6': ensure => 'present', source => 'puppet:///modules/kazoo/RPM-GPG-KEY-EPEL-6' }
-	
+
   file { '/selinux/enforce': ensure => 'absent' }
   
   exec { 'yum-clean-all':
